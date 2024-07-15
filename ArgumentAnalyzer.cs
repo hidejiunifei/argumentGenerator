699be3500955bcb5abf23cd9d23eb9b6a5cdf4f7
Hideji Hayakawa - SYSMAP <hideji.hayakawa.sysmap@araujo.com.br>
2024-07-14 23:34:35 -0300

    change analyzer message and add support to implicit creation

--- ArgumentAnalyzer.cs
+++ ArgumentAnalyzer.cs
@@ -15,7 +15,7 @@ namespace ArgumentGenerator
     {
         public const string DiagnosticId = "ArgumentAnalyzer";
         internal static readonly LocalizableString Title = "ArgumentAnalyzer Title";
-        internal static readonly LocalizableString MessageFormat = "ArgumentAnalyzer '{0}'";
+        internal static readonly LocalizableString MessageFormat = "Parameter list empty";
         internal const string Category = "ArgumentAnalyzer Category";
 
         internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
@@ -30,11 +30,13 @@ namespace ArgumentGenerator
 
         private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
+            
             var localDeclaration = (ArgumentListSyntax)context.Node;
 
             if (!localDeclaration.Arguments.Any())
             {
-                if (context.Node.Parent is ObjectCreationExpressionSyntax || context.Node.Parent is InvocationExpressionSyntax)
+                if (context.Node.Parent is ObjectCreationExpressionSyntax || context.Node.Parent is InvocationExpressionSyntax ||
+                    context.Node.Parent is ImplicitObjectCreationExpressionSyntax)
                 {
                     var diagnostic = Diagnostic.Create(Rule, localDeclaration.GetLocation());
 
