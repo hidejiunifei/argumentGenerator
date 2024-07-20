2ec8957f943cf491cee7f11279e512a27b8552a2
hideji <hidejiunifei@gmail.com>
2024-07-20 10:07:40 -0300

    unused usings removed

--- ArgumentAnalyzer.cs
+++ ArgumentAnalyzer.cs
@@ -2,11 +2,7 @@
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Threading;
 
 namespace ArgumentGenerator
 {
